82378ddd4bc2c17e319345e3b27527608f162ef7
gabriel <gr8577577@gmail.com>
2026-05-22 19:34:22 -0300

    variáveis expostas

--- backend-app/src/main/java/com/gestao_restaurante/controller/ClienteController.java
+++ backend-app/src/main/java/com/gestao_restaurante/controller/ClienteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/xrest")
 @CrossOrigin(origins = "*")
 public class ClienteController {
